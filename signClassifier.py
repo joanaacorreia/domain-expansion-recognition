a7527d3c6b06dc756599282fdf914c2ada0229bb
joanaacorreia <joana.ruivaco@gmail.com>
2026-03-18 14:37:20 +0000

    feat: unlimited void detection and refactor helper functions

--- signClassifier.py
+++ signClassifier.py
@@ -1,3 +1,9 @@
+# hand landmark anatomy reference:
+# fingertips = tipId
+# distal interphalangeal joints = dipId
+# proximal interphalangeal joints = pipId
+# metacarpophalangeal joints = mcpId
+
 class SignClassifier:
     def __init__(self):
         self.signs = {
@@ -20,29 +26,41 @@ class SignClassifier:
                 return sign
             
         return None
+    
         
+    def isFingerUp(self, landmarks, tipId, pipId):
+        tipY = landmarks[tipId][1]
+        pipY = landmarks[pipId][1]
         
-    def isFingerUp(self, landmarks, fingerTipId, knuckleId):
-        return landmarks[fingerTipId][1] < landmarks[knuckleId][1]
+        return tipY < pipY
     
     
-    def isThumbUp(self, landmarks, fingerTipId, knuckleId):
-        return landmarks[fingerTipId][0] > landmarks[knuckleId][0]
+    def isThumbUp(self, landmarks, tipId, pipId):
+        tipX = landmarks[tipId][0]
+        pipX = landmarks[pipId][0]
+        
+        return tipX > pipX
     
+    
+    def isFingerBent(self, landmarks, tipId, dipId, mcpId):
+        tipY = landmarks[tipId][1]
+        dipY = landmarks[dipId][1]
+        mcpY = landmarks[mcpId][1]
+        
+        return tipY > dipY and tipY < mcpY
+        
     
     def detectMalevolentShrine(self, hands): 
-        # get the first hand
+        # sign is symmetrical, only need one hand
         hand = hands[0]
         
-        # index and pinky fingers down
+        # index and pinky down
         indexDown = not self.isFingerUp(hand, 8, 6) 
         pinkyDown = not self.isFingerUp(hand, 20, 18)
         
-        # middle and ring fingers up
+        # middle, ring, and thumb up
         middleUp = self.isFingerUp(hand, 12, 10)
         ringUp = self.isFingerUp(hand, 16, 14)
-        
-        # thumb up 
         thumbUp = self.isThumbUp(hand, 4, 2)
         
         return all([
@@ -55,8 +73,28 @@ class SignClassifier:
     
     
     def detectUnlimitedVoid(self, hands):
-        pass
-    
+        # gojo's sign uses one hand only
+        hand = hands[0]
+        
+        # index and thumb up
+        indexUp = self.isFingerUp(hand, 8, 6)
+        thumbUp = self.isThumbUp(hand, 4, 2)
+        
+        # ring and pinky down
+        ringDown = not self.isFingerUp(hand, 16, 14)
+        pinkyDown = not self.isFingerUp(hand, 20, 18)
+        
+        # middle bent
+        middleBent = self.isFingerBent(hand, 12, 11, 9)
+        
+        return all([
+            indexUp, 
+            thumbUp, 
+            ringDown, 
+            pinkyDown,
+            middleBent
+        ])
+
     
     def detectChimeraShadowGarden(self, hands):
         pass
